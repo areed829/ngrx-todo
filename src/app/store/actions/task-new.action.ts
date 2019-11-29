34d72af2a1350aa3df162bcdbfe873adaf3c73ea
Alex Reed <alexanderreed@Alexanders-MacBook-Pro.local>
2019-11-29 14:26:59 -0500

    setup actions

---
+++ src/app/store/actions/task-new.action.ts
@@ -0,0 +1,43 @@
+import { createAction, props } from '@ngrx/store';
+import { Task } from '../../models';
+
+// Load Tasks
+export const LoadTasks = createAction('[Task] Load Tasks');
+export const LoadTasksFail = createAction(
+  '[Task] Load Tasks Fail',
+  props<{ error: any }>()
+);
+export const LoadTasksSucces = createAction(
+  '[Task] Load Tasks Success',
+  props<{ tasks: Task[] }>()
+);
+
+// Add Tasks
+export const AddTask = createAction('[Task] Add Task', props<{ task: Task }>());
+export const AddTaskFail = createAction(
+  '[Task] Add TaskFail',
+  props<{ task: Task; error: any }>()
+);
+export const AddTaskSucces = createAction('[Task] Add Task Success');
+
+// Update Tasks
+export const UpdateTask = createAction(
+  '[Task] Update Task',
+  props<{ task: Task }>()
+);
+export const UpdateTaskFail = createAction(
+  '[Task] Update Task Fail',
+  props<{ task: Task; error: any }>()
+);
+export const UpdateTaskSuccess = createAction('[Task] Update Task Success');
+
+// Delete Tasks
+export const DeleteTask = createAction(
+  '[Task Delete Task]',
+  props<{ task: Task }>()
+);
+export const DeleteTaskFail = createAction(
+  '[Task Delete Task]',
+  props<{ task: Task; error: any }>()
+);
+export const DeleteTaskSuccess = createAction('[Task Delete Task]');
