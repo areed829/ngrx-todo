06b420462ebec1a301421563e17e94ff4217f3ee
Alex <areed829@gmail.com>
2019-11-30 18:36:16 -0500

    move selectors to snippets

--- src/app/store/selectors/task.selectors.ts
+++ src/app/store/selectors/task.selectors.ts
@@ -1,28 +1,3 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { taskFeatureKey, TaskState } from '../reducers';
 // ngFeatureSelector, ngTaskSelector, ngLoadingStateSelector, ngCompleted
-
-export const taskFeatureSelector = createFeatureSelector(taskFeatureKey);
-
-export const getTasks = createSelector(
-  taskFeatureSelector,
-  (state: TaskState) => state.tasks
-);
-
-export const getTaskLoading = createSelector(
-  taskFeatureSelector,
-  (state: TaskState) => state.loading
-);
-
-export const getTaskLoaded = createSelector(
-  taskFeatureSelector,
-  (state: TaskState) => state.loaded
-);
-
-export const getUncompletedTasks = createSelector(getTasks, tasks =>
-  tasks.filter(task => !task.completed)
-);
-
-export const getCompletedTasks = createSelector(getTasks, tasks =>
-  tasks.filter(task => task.completed)
-);
